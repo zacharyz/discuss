c0f4f3b1ac03cfa68b840378d94818282e1b6537
Zachary Zadell <zacharyz@gmail.com>
2024-04-24 13:04:39 -0700

    search action fetch posts by search term new search page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,38 @@
 import Link from "next/link";
 import {
-    Navbar,
-    NavbarBrand,
-    NavbarContent,
-    NavbarItem,
-    Input,
-    Button,
-    Avatar,
-    Popover,
-    PopoverContent,
-    PopoverTrigger
-} from '@nextui-org/react';
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import SearchInput from "./search-input";
 import HeaderAuth from "./header-auth";
+import { Suspense } from "react";
 
 export default async function Header() {
-
-
-   
-    return (
-        <Navbar className="shadow mb-6">
-            <NavbarBrand>
-                <Link href="/" className="font-bold">Discuss</Link>
-            </NavbarBrand>
-            <NavbarContent justify="center">
-                <NavbarItem>
-                    <Input />
-                </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <HeaderAuth />
-            </NavbarContent>
-        </Navbar>
-    )
-    
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Suspense>
+            <SearchInput />
+          </Suspense>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
+    </Navbar>
+  );
 }
--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -7,6 +7,19 @@ export type PostWithData = Post & {
   user: { name: string };
 };
 
+export function fetchPostsBySearchTerm(term: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+    where: {
+      OR: [{ title: { contains: term } }, { content: { contains: term } }],
+    },
+  });
+}
+
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
   return db.post.findMany({
     where: {
