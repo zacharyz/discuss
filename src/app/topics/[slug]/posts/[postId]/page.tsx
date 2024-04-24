9c5d4db78c4f0fd10f9299b7a142228996791633
Zachary Zadell <zacharyz@gmail.com>
2024-04-24 10:28:06 -0700

    added nextui skeleton to post show

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
+import { Suspense } from "react";
 import PostShow from "@/components/posts/post-show";
 import CommentList from "@/components/comments/comment-list";
 import CommentCreateForm from "@/components/comments/comment-create-form";
+import PostShowLoading from "@/components/posts/post-show-loading";
 import paths from "@/paths";
 
 interface PostShowPageProps {
@@ -19,9 +21,13 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
-      <CommentList postId={postId} />
+      <Suspense fallback={<div>loading...</div>}>
+        <CommentList postId={postId} />
+      </Suspense>
     </div>
   );
 }
--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,22 +1,20 @@
 import type { Comment } from "@prisma/client";
 import { db } from "@/db";
-import {cache} from 'react';
+import { cache } from "react";
 
-export type CommentWithAuthor = (
-    Comment & {
-        user: {name: string | null; image: string | null; }
-    }
-);
+export type CommentWithAuthor = Comment & {
+  user: { name: string | null; image: string | null };
+};
 
-export const fetchCommentsByPostId = cache((postId: string): Promise<CommentWithAuthor[]> => {
-      console.log("making a query for comments!");
-
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
     return db.comment.findMany({
-        where: {
-            postId: postId,
-        },
-        include: {
-            user: { select: {name: true, image: true}},
-        }
+      where: {
+        postId: postId,
+      },
+      include: {
+        user: { select: { name: true, image: true } },
+      },
     });
-});
+  }
+);
