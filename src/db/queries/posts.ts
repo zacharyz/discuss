fe149d5786a716ff80a473e29077fd78e1c97f4e
Zachary Zadell <zacharyz@gmail.com>
2024-04-24 10:49:24 -0700

    fetch and display top posts on the home page

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,23 +1,39 @@
 import type { Post } from "@prisma/client";
 import { db } from "@/db";
 
-export type PostWithData = (
-    Post & {
-        topic: { slug: string};
-        _count: { comments: number};
-        user: {name: string}
-    }
-);
+export type PostWithData = Post & {
+  topic: { slug: string };
+  _count: { comments: number };
+  user: { name: string };
+};
 
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
-    return db.post.findMany({
-        where: {
-            topic: {slug}
+  return db.post.findMany({
+    where: {
+      topic: { slug },
+    },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: [
+      {
+        comments: {
+          _count: "desc",
         },
-        include: {
-            topic: { select: { slug: true}},
-            user: { select: {name: true}},
-            _count: { select: {comments: true}}
-        }
-    });
+      },
+    ],
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    take: 5,
+  });
 }
